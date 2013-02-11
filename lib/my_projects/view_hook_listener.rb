468e463f2e5aa1e811152fa2bf78791aaa03c7f3
Nick Peelman <nick@peelman.us>
2013-02-11 12:46:41 -0500

    Localizing.

--- lib/my_projects/view_hook_listener.rb
+++ lib/my_projects/view_hook_listener.rb
@@ -6,15 +6,15 @@ class MyProjects < Redmine::Hook::ViewListener
 
   def load_projects()
       html = '<div class="box projects" id="statuses">'
-      html += '<h3 class="">My Projects</h3><ul>'
+      html += '<h3 class="">' + l(:label_my_projects) + '</h3><ul>'
       projects  = Project.visible(User.current).find(:all, :order => "projects.name")
       admin_projects = []
       projects.each do |project|
         if User.current.member_of?(project)
           html += '<li>'
           html += "#{link_to h(project.name), :controller => 'projects', :action => 'show', :id => project }"
-          html += " | #{link_to 'Issues', :controller => 'issues', :action=>'index', :project_id => project } "
-          html += " | #{link_to 'Wiki', :controller => 'wiki', :action=>'show', :project_id => project } "
+          html += " | #{link_to l(:label_issue_plural), :controller => 'issues', :action=>'index', :project_id => project } "
+          html += " | #{link_to l(:label_wiki), :controller => 'wiki', :action=>'show', :project_id => project } "
           html += '</li>'
         else
           admin_projects << project
@@ -23,12 +23,12 @@ class MyProjects < Redmine::Hook::ViewListener
       html += '</ul>'
 
       if User.current.admin?
-        html += '<h3>Admin Projects</h3><ul>'
+        html += '<h3>' + l(:label_admin_projects) + '</h3><ul>'
         admin_projects.each do |project|
           html += '<li>'
           html += "#{link_to h(project.name), :controller => 'projects', :action => 'show', :id => project }"
-          html += " | #{link_to 'Issues', :controller => 'issues', :action=>'index', :project_id => project } "
-          html += " | #{link_to 'Wiki', :controller => 'wiki', :action=>'show', :project_id => project } "
+          html += " | #{link_to l(:label_issue_plural), :controller => 'issues', :action=>'index', :project_id => project } "
+          html += " | #{link_to l(:label_wiki), :controller => 'wiki', :action=>'show', :project_id => project } "
           html += '</li>'
         end
         html += '</ul>'
