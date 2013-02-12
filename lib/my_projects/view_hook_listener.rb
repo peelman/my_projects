20c2b01ad982043a6aacd4228c78ec9911f2432c
Nick Peelman <nick@peelman.us>
2013-02-12 00:32:20 -0500

    Gracefully handle when the user has no projects assigned. Move things around.

--- lib/my_projects/view_hook_listener.rb
+++ lib/my_projects/view_hook_listener.rb
@@ -6,35 +6,54 @@ class MyProjects < Redmine::Hook::ViewListener
 
   def load_projects()
       html = '<div class="box projects" id="statuses">'
-      html += '<h3 class="">' + l(:label_my_projects) + '</h3><ul>'
-      projects  = Project.visible(User.current).find(:all, :order => "projects.name")
+      html += '<h3 class="">' + l(:label_my_projects) + '</h3>'
+
+      all_projects  = Project.visible().find(:all, :order => "projects.name")
       admin_projects = []
-      projects.each do |project|
+      my_projects = []
+
+      all_projects.each do |project|
         if User.current.member_of?(project)
-          html += '<li>'
-          html += "#{link_to h(project.name), :controller => 'projects', :action => 'show', :id => project }"
-          html += " | #{link_to l(:label_issue_plural), :controller => 'issues', :action=>'index', :project_id => project } "
-          html += " | #{link_to l(:label_wiki), :controller => 'wiki', :action=>'show', :project_id => project } "
-          html += '</li>'
+          my_projects << project
         else
           admin_projects << project
         end
       end
-      html += '</ul>'
 
-      if User.current.admin?
-        html += '<h3>' + l(:label_admin_projects) + '</h3><ul>'
+      if my_projects.first
+        html += '<ul>'
+        my_projects.each do |project|
+          html += link_to_project(project)
+        end
+        html += '</ul>'
+      else
+        html += '<p class="nodata">' + l(:label_no_data) + '</p>'
+      end
+
+      if User.current.admin? && admin_projects.first
+        html += '<h3>' + l(:label_admin_projects) + '</h3>'
+        html += '<ul>'
         admin_projects.each do |project|
-          html += '<li>'
-          html += "#{link_to h(project.name), :controller => 'projects', :action => 'show', :id => project }"
-          html += " | #{link_to l(:label_issue_plural), :controller => 'issues', :action=>'index', :project_id => project } "
-          html += " | #{link_to l(:label_wiki), :controller => 'wiki', :action=>'show', :project_id => project } "
-          html += '</li>'
+          html += link_to_project(project)
         end
         html += '</ul>'
       end
       html += '</div>'
+      
+      html += "<div> #{User.current.projects_by_role}</div>"
+      
       return html
   end
+  
+  def link_to_project(project)
+    html = '<li>'
+    html += "#{link_to h(project.name), :controller => 'projects', :action => 'show', :id => project }"
+    html += " | #{link_to l(:label_issue_plural), :controller => 'issues', :action=>'index', :project_id => project } "
+    html += " | #{link_to l(:label_wiki), :controller => 'wiki', :action=>'show', :project_id => project } "
+    html += '</li>'
+    
+    return html
+  end
+    
 end
 
