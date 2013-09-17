182717fa7865009ea41009b7172cb9e4183299da
Nick Peelman <nick@peelman.us>
2013-09-17 12:15:20 -0400

    Attempted to alleviate #5 (base path suffix problem)

--- lib/my_projects/view_hook_listener.rb
+++ lib/my_projects/view_hook_listener.rb
@@ -44,9 +44,9 @@ class MyProjects < Redmine::Hook::ViewListener
   
   def link_to_project(project)
     html = '<li>'
-    html += "#{link_to h(project.name), :controller => 'projects', :action => 'show', :id => project }"
-    html += " | #{link_to l(:label_issue_plural), :controller => 'issues', :action=>'index', :project_id => project } "
-    html += " | #{link_to l(:label_wiki), :controller => 'wiki', :action=>'show', :project_id => project } "
+    html += "#{link_to h(project.name), { :controller => 'projects', :action => 'show', :id => project } }"
+    html += " | #{link_to l(:label_issue_plural), { :controller => 'issues', :action=>'index', :project_id => project } } "
+    html += " | #{link_to l(:label_wiki), { :controller => 'wiki', :action=>'show', :project_id => project } } "
     html += '</li>'
     
     return html
